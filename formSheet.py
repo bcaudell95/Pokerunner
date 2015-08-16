1f686e257bd9cb2b46da14fec66d9f16c3a99c51
bcaudell95 <bcaudell95@gmail.com>
2015-08-15 19:14:06 -0500

    clean up formSheet.py

--- formSheet.py
+++ formSheet.py
@@ -2,15 +2,21 @@ import spriteSheet
 
 class FormSheet(object):
 
-    FRAME_DIMENSIONS = (256, 128)    # Dimensions for one image
-    ROWS = 8                        # Number of images per sprite sheet
-    SHEET_DIMENSIONS = (FRAME_DIMENSIONS[0], FRAME_DIMENSIONS[1]*ROWS)  #Dimensions for a sprite sheet
-    STATES_COUNT = 2                # Number of sheets per image
-    
-    def __init__(self, filename):
-        self.sheets = []
-        for i in range(FormSheet.STATES_COUNT):
-            self.sheets.append(spriteSheet.spritesheet(filename, FormSheet.SHEET_DIMENSIONS, i*FormSheet.SHEET_DIMENSIONS[1], FormSheet.ROWS))
-
-    def getStateSheet(self, i):
-        return self.sheets[i]
+	FRAME_DIMENSIONS = (256, 128)   # Dimensions for one image
+	ROWS = 8                        # Number of images per sprite sheet
+	SHEET_DIMENSIONS = (FRAME_DIMENSIONS[0], FRAME_DIMENSIONS[1]*ROWS)  #Dimensions for a sprite sheet
+	STATES_COUNT = 2                # Number of sheets per image
+	
+	def __init__(self, filename):
+		self.filename = filename
+		self.sheets = []
+		for i in range(FormSheet.STATES_COUNT):
+			self.loadSheetByIndex(i)
+	
+	def loadSheetByIndex(self, index):
+		fileData = spriteSheet.SpriteSheetFile(self.filename, index*FormSheet.SHEET_DIMENSIONS[1])
+		dimensions = spriteSheet.SpriteSheetDimensions(FormSheet.SHEET_DIMENSIONS, FormSheet.ROWS)
+		self.sheets.append(spriteSheet.spritesheet(fileData, dimensions))
+	
+	def getStateSheet(self, i):
+		return self.sheets[i]
