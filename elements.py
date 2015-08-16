78bf8e58fceacb4dd822ddd2dea84ffd03845afd
bcaudell95 <bcaudell95@gmail.com>
2015-08-15 19:51:04 -0500

    fix indentation style of elements.py

--- elements.py
+++ elements.py
@@ -1,11 +1,11 @@
 from enum import Enum
 
 class Elements(Enum):
-    NORMAL = 0
+	NORMAL = 0
 
 def getElementSheetFile(element):
-    imagesDirectory = 'assets/images/'
-    if Elements.NORMAL.name == element:
-        return imagesDirectory + 'runningcat_50.png'
-    else:
-        return ''
+	imagesDirectory = 'assets/images/'
+	if Elements.NORMAL.name == element:
+		return imagesDirectory + 'runningcat_50.png'
+	else:
+		return ''
