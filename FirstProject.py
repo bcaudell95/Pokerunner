14b1b3b8addcd48b93fc14c36b7d23d37fd57088
bcaudell95 <bcaudell95@gmail.com>
2015-08-15 21:14:58 -0500

    clean up FirstProject.py

--- FirstProject.py
+++ FirstProject.py
@@ -2,29 +2,49 @@ import pygame
 import display
 import player
 
-pygame.init()
+class Pokerunner:
 
-clock = pygame.time.Clock()
-display = display.Display()
-myPlayer = player.Player()
+	FRAMES_PER_SECOND = 30
 
-gameExit = False
+	def __init__(self):
+		pygame.init()
 
-while not gameExit:
-	#Handle Events
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			gameExit = True
-		elif event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_UP:
-				myPlayer.changeMovementState(player.MovementStates.JUMPING)
-			elif event.key == pygame.K_DOWN:
-				myPlayer.changeMovementState(player.MovementStates.RUNNING)
+		self.clock = pygame.time.Clock()
+		self.display = display.Display()
+		self.player = player.Player()
 
-	myPlayer.stepFrame()
-	display.updatePlayerData(myPlayer.getCurrentPlayerState())
-	display.updateScreen()
-	clock.tick(30)
-	
-pygame.quit()
-quit()
+		self.gameExit = False
+		
+		self.mainLoop()
+
+	def mainLoop(self):
+		while not self.gameExit:
+			self.handleEvents()
+			
+			self.player.stepFrame()
+			self.display.updatePlayerData(self.player.getCurrentPlayerState())
+			self.display.updateScreen()
+			self.clock.tick(Pokerunner.FRAMES_PER_SECOND)
+
+	def handleEvents(self):
+		for event in pygame.event.get():
+			if self.isEventQuit(event):
+				self.gameExit = True
+			elif self.isEventKeyDown(event):
+				self.handleKeyEvent(event)
+					
+	def isEventQuit(self, event):
+		return event.type == pygame.QUIT
+		
+	def isEventKeyDown(self, event):
+		return event.type == pygame.KEYDOWN
+		
+	def handleKeyEvent(self, event):
+		if event.key == pygame.K_UP:
+			self.player.changeMovementState(player.MovementStates.JUMPING)
+					
+	def endGame(self):
+		pygame.quit()
+		quit()
+
+Pokerunner()
