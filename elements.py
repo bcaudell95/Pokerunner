89009b401f8ed10ef694e12d741974cf803565f2
bcaudell95 <bcaudell95@gmail.com>
2015-08-15 20:16:50 -0500

    fix element filename dictionary lookup

--- elements.py
+++ elements.py
@@ -7,4 +7,7 @@ elementImagesDict = {'NORMAL': 'runningcat_50.png'}
 
 def getElementSheetFile(element):
 	imagesDirectory = 'assets/images/'
-	return imagesDirectory + elementImagesDict[element]
+	if element in elementImagesDict.keys():
+		return imagesDirectory + elementImagesDict[element]
+	else:
+		return ''
